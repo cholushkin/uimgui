a8614a8116296d225caf9290f7693b992d89a9cc
max <cholushkin@gmail.com>
2023-07-05 14:31:33 +0300

    style: edit white-spaces

--- Source/ImGUIExtension.cs
+++ Source/ImGUIExtension.cs
@@ -1,20 +1,21 @@
 using ImGuiNET;
 
-namespace UImGui
+
+namespace UImGui 
 {
-	public static class ImGUIExtension
-	{
-		public static void HelpMarker(string description)
-		{
-			ImGui.TextDisabled("[?]");
-			if (ImGui.IsItemHovered())
-			{
-				ImGui.BeginTooltip();
-				ImGui.PushTextWrapPos(ImGui.GetFontSize() * 35.0f);
-				ImGui.TextUnformatted(description);
-				ImGui.PopTextWrapPos();
-				ImGui.EndTooltip();
-			}
-		}
-	}
+    public static class ImGUIExtension // todo: rename class (there is with the same name in in utils )
+    {
+        public static void HelpMarker(string description)
+        {
+            ImGui.TextDisabled("[?]");
+            if (ImGui.IsItemHovered())
+            {
+                ImGui.BeginTooltip();
+                ImGui.PushTextWrapPos(ImGui.GetFontSize() * 35.0f);
+                ImGui.TextUnformatted(description);
+                ImGui.PopTextWrapPos();
+                ImGui.EndTooltip();
+            }
+        }
+    }
 }
